0bd5ff41fc105211f278df08d1964dc38a04dfb5
DRockmore89 <donaldrockmore@yahoo.com>
2021-04-17 18:44:17 -0400

    generate README

--- Develop/index.js
+++ Develop/index.js
@@ -1,38 +1,77 @@
 // TODO: Include packages needed for this application
 
-const inquirer = require("inquirer");
-// inquirer
-// .prompt([
-//     /* Pass your questions in here */
-//   ])
-//   .then(answers => {
-//     // Use user feedback for... whatever!!
-//   })
-//   .catch(error => {
-//     if(error.isTtyError) {
-//       // Prompt couldn't be rendered in the current environment
-//     } else {
-//       // Something else went wrong
-//     }
-//   });
+const inquirer = require('inquirer');
+const fs = require('fs');
+const _ = require('lodash');
+const markdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [
+
     {
         type: 'input',
         name: 'github',
         message: 'What is your github username?',
-    }
+    },
+    {
+        type: 'input',
+        name: 'projectName',
+        message: 'what is your project name?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Select a licence for your project?',
+        choices: ['GPL', 'MIT', 'LGPL']
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'enter all the name your projects used in your application?',
+    },
+    {
+        type: 'input',
+        name: 'projectDescription',
+        message: 'give me a description of your project?',
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'show or describe how to instll your project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'show or descibe how to use your project?',
+    },
 ];
-{github: "DRockmore89"}
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if(err) {
+            return console.log(err);
+        }
+        console.log('The Readme.md file has been generated successfully.')
+
+    })
+        
+
+};
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then.
-}
+    inquirer.prompt(questions) 
+    .then((data) => {
+        fs.writeFile(`README.md`, `markdown`, err => {
+        err ? console.log(err) : console.log(`Write File Success!`)},
+          );     
+        })
+    
+};
+
 
 // Function call to initialize app
-init();
+
+init()
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,90 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'GNU AGPLv3':
+      return `https://choosealicense.com/licenses/agpl-3.0/`;
+    case 'GNU GPLv3':
+      return `https://choosealicense.com/licenses/gpl-3.0/`;
+    case 'MIT':
+      return `https://choosealicense.com/licenses/mit/`;   
+    case 'Apache license 2.0':
+      return `https://choosealicense.com/licenses/apache-2.0/`;
+    case 'Mozilla Public License 2.0':
+      return `https://choosealicense.com/licenses/mpl-2.0/`;
+    default:
+      return ''
+          
+    }
+ 
+
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'GNU AGPLv3':
+      return `https://choosealicense.com/licenses/agpl-3.0/`;
+    case 'GNU GPLv3':
+      return `https://choosealicense.com/licenses/gpl-3.0/`;
+    case 'MIT':
+      return `https://choosealicense.com/licenses/mit/`;   
+    case 'Apache license 2.0':
+      return `https://choosealicense.com/licenses/apache-2.0/`;
+    case 'Mozilla Public License 2.0':
+      return `https://choosealicense.com/licenses/mpl-2.0/`;
+    default:
+      return ''
+          
+    }
+}
+
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  switch (license) {
+    case 'GNU AGPLv3':
+      return licenseText.AGPLv3;
+    case 'GNU GPLv3':
+      return licenseText.GPLv3;
+    case 'MIT':
+      return licenseText.MIT;   
+    case 'Apache license 2.0':
+      return licenseText.Apache;
+    case 'Mozilla Public License 2.0':
+      return licenseText.Mozilla
+    default:
+      return ''
+          
+    }
+}
+
+// gitt
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answers) {
+  let licenseBadge = renderLicenseBadge(answers.projectlicense);
+  let licenseLink = renderLicenseLink(answers.projectlicense);
+  let licenseSection = renderLicenseSection(answers.projectlicense);
+  let tableofcontent = renderTableofcontent(answers);
+  return `# ${answers.projectName}\n
+  ![${answers.projectlicense}](${licenseBadge})\n
+  [Repository](${answers.projectRepo})\n
+  ## Table of Content\n ${tableofcontent}\n
+  ## Description\n > ${answers.projectDescription}\n
+  ##Installation\n > ${answers.projectInstallation}\n
+  ##usage\n > ${answers.projectUsage}\n
+  ##License\n > [${answers.projectLicense}](${licenseLink})\n
+  ${licenseSection}\n
+  ##Contact\n > Please contact me for any questions about projects or work
+
+
   
+
 `;
 }
 
